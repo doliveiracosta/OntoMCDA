f8cf3793f4a53813d7e670b63e146bfb7192f9eb
doliveiracosta <david.oliveira@upe.br>
2026-05-17 14:32:07 -0300

    Delete ontomcda directory

--- ontomcda/__init__.py
+++
@@ -1,7 +0,0 @@
-"""OntoMCDA semantic recommendation package."""
-
-from .ontology import load_profiles
-from .recommender import RecommendationResult, recommend_methods
-from .text_inference import infer_premises
-
-__all__ = ["RecommendationResult", "infer_premises", "load_profiles", "recommend_methods"]
